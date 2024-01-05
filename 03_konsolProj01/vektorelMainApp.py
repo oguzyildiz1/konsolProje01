bdd986d5268cdd04d0ee6e2c3a6869c8dade29c1
Oğuzhan Yıldız <150700256+oguzyildiz1@users.noreply.github.com>
2024-01-05 17:03:41 +0300

    Vektörel bilisim proje 1 Konsol programları

---
+++ 03_konsolProj01/vektorelMainApp.py
@@ -0,0 +1,34 @@
+#test app for main app
+#main app is typing a wrong input
+
+import havadurumu.havalar
+import hesapmakinesi.hesaplama
+import sicaklikcevirme.sicaklik
+import nothesaplama.notHesap
+
+def anamenu():
+    print("╔" + "═"*25 +"╗")
+    print("║\tGenel App\t  ║")
+    print("║   1. Hava durumu\t  ║")
+    print("║   2. Hesap makinesi\t  ║")
+    print("║   3. Sıcaklık çevirme   ║")
+    print("║   4. Not hesaplama      ║")
+    print("╠"+ "═"*25 + "╣")
+    print("║Seçiminiz: \t\t  ║")
+    print("╚" + "═"*25 +"╝")
+    secim1 = input()
+    if secim1 == "1":
+        havadurumu.havalar.hdmenu()
+        anamenu()
+    elif secim1 == "2":
+        hesapmakinesi.hesaplama.hmmenu()
+        anamenu()
+    elif secim1 == "3":
+        sicaklikcevirme.sicaklik.scmenu()
+        anamenu()
+    elif secim1 == "4":
+        nothesaplama.notHesap.notMenu()
+        anamenu()
+    #elif secim1 == "Q":
+    #    exit()
+anamenu()
