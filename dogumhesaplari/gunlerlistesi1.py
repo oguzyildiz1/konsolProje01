c670ebe23e1971be42765785d452886750d45f76
oguzyildiz1 <yildizoguz1@gmail.com>
2024-01-18 13:34:22 +0300

    zodiac signs updated

--- dogumhesaplari/gunlerlistesi1.py
+++ dogumhesaplari/gunlerlistesi1.py
@@ -4,4 +4,17 @@ def setDaysTurkish():
               "Sunday" : "Pazar"}
     
     return gunler
-          
+
+
+def setZodiacSign():
+    signs = {
+        "Koç" : ["21 Mart", "20 Nisan"],
+        "Boğa" : ["21 Nisan", "20 Mayıs"],
+        "İkizler" : ["21 Mayıs", "21 Haziran"],
+        "Yengeç" : ["22 Haziran", "22 Temmuz"],
+        "Aslan" : ["23 Temmuz", "22 Ağustos"],
+        "Başak" : ["23 Ağustos", "22 Eylül"],
+        "Terazi" : ["23 Eylül", "23 Ekim"],
+        "Akrep" : ["24 Ekim", "22 Kasım"],
+        "Yay" : ["23 Kasım", "21 Aralık"],
+            }
